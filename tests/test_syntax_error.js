6fc4d2ee06f060e644c9698cb9637b7e60dc3e5d
Brett Zamir <brettz9@yahoo.com>
2019-10-05 09:16:49 +0800

    test(syntax‑error): use `readFixtureSync` utility, and in erring test results, escape line breaks and use fixture's project-relative file path
    Co-Authored-By: Brett Zamir <brettz9@yahoo.com>
    Co-Authored-By: ExE Boss <3889017+ExE-Boss@users.noreply.github.com>

--- tests/test_syntax_error.js
+++ tests/test_syntax_error.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
 const util = require('util');
 const Parser = require('../lib/parsing.js');
+const {readFixtureSync} = require('./utils.js');
 
+/** @type {{[fixtureName: string]: import('./utils').Fixture}} */
 const Fixtures = {
   TYPESCRIPT: readFixtureSync('erring-typescript-types'),
 };
@@ -12,7 +12,8 @@ const Fixtures = {
 describe('Parser', function() {
   it('should throw errors when parsing erring tests/fixtures/*', function() {
     Object.keys(Fixtures).forEach(function(fixtureName) {
-      Fixtures[fixtureName].forEach(function({skip, typeExprStr, position}) {
+      const fixture = Fixtures[fixtureName];
+      fixture.forEach(function({skip, typeExprStr, position}) {
         if (skip) return;
 
         let parsed;
@@ -23,35 +24,16 @@ describe('Parser', function() {
           return;
         }
 
-        const debugMessage = util.format('Should have erred parsing %s at %s:%d; instead parsed as:\n\n%s',
-                                       typeExprStr,
-                                       position.filePath,
-                                       position.lineno,
-                                       JSON.stringify(parsed, null,2));
+        const debugMessage = util.format(
+          'Should have erred parsing %s at %s:%d; instead parsed as:\n\n%s',
+          typeExprStr.replace(/\n/g, '\\n'),
+          fixture.filePath,
+          position.lineno,
+          JSON.stringify(parsed, null, 2)
+        );
 
         throw new Error(debugMessage);
       });
     });
   });
 });
-
-
-function readFixtureSync(fileName) {
-  const filePath = path.resolve(__dirname, 'fixtures', fileName);
-
-  return fs.readFileSync(filePath, 'utf8')
-    .trim()
-    .split(/\n/)
-    .map(function(line, lineIdx) {
-      return {
-        // When the line starts with "//", we should skip it.
-        skip: /^\/\//.test(line),
-
-        typeExprStr: line.trim().replace(/^\{(.*)\}$/, '$1').replace(/\\n/g, '\n'),
-        position: {
-          filePath,
-          lineno: lineIdx + 1,
-        },
-      };
-    });
-}
